3fb20a1290c7a02b77ff040bac210167743ca80b
Ajaj Rajguru <developer@kodework.com>
2020-08-05 12:57:49 +0530

    Handled utlities section and updated some common functions

--- Living - Styleguide/js/main.js
+++ Living - Styleguide/js/main.js
@@ -4,28 +4,32 @@ jQuery(document).ready(function($){
 	COLOR SWATCH
 	********************/
 	//convert rgba color to hex color
-	$.cssHooks.backgroundColor = {
-	    get: function(elem) {
-	        if (elem.currentStyle)
-	            var bg = elem.currentStyle["background-color"];
-	        else if (window.getComputedStyle)
-	            var bg = document.defaultView.getComputedStyle(elem,
-	                null).getPropertyValue("background-color");
-	        if (bg.search("rgb") == -1)
-	            return bg;
-	        else {
-	            bg = bg.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-	            function hex(x) {
-	                return ("0" + parseInt(x).toString(16)).slice(-2);
-	            }
-	            return "#" + hex(bg[1]) + hex(bg[2]) + hex(bg[3]);
-	        }
-	    }
-	}
+	 $.cssHooks.backgroundColor = {
+		get: function (elem) {
+			if (elem.currentStyle)
+				var bg = elem.currentStyle["backgroundColor"];
+			else if (window.getComputedStyle)
+				var bg = document.defaultView.getComputedStyle(elem,
+				null).getPropertyValue("background-color");
+			if (bg.search('rgba') > -1) {
+				return '#00ffffff';
+			} else {
+				if (bg.search('rgb') == -1) {
+					return bg;
+				} else {
+					bg = bg.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+				function hex(x) {
+					return ("0" + parseInt(x).toString(16)).slice(-2);
+				}
+					return "#" + hex(bg[1]) + hex(bg[2]) + hex(bg[3]);
+				}
+			}
+		}
+	};
 	//set a label for each color swatch
 	$('.cd-color-swatch').each(function(){
 		var actual = $(this);
-		$('<b class="copy">'+actual.css("background-color")+'</b>').insertAfter(actual);
+		$('<b class="copy">'+actual.css("backgroundColor")+'</b>').insertAfter(actual);
 	});
 
 
@@ -53,7 +57,7 @@ jQuery(document).ready(function($){
 				spanElement.appendTo(wrapperElement);
 				wrapperElement.appendTo(containerHtml);
 				wrapperElement.append('"'+block2[1]+'&lt;/' + structure + '&gt;');
-				console.log($(value));
+				//console.log($(value));
 			}
 		});
 	}
@@ -62,19 +66,17 @@ jQuery(document).ready(function($){
 
 	/*Button Dimensions for clear size visibility*/
 	$('#buttons .cd-box button').each(function(idx,value){
+		$(value).attr('data-bg', $(value).css('background-color'));
 		var cdStructure = $('.cd-box .output'),
-			//btnBG  = $(value).css('background-color');
-			btnFs  = $(value).css('font-size'),
-			btnLh  = $(value).css('line-height'),
-			btnheight  = $(value).css('height'),
-			btnWidth  = $(value).css('width');
-			$(value).mouseover(function() {
-				$(cdStructure).text('');
-				$(cdStructure).append('Font-size- ' + btnFs + ' Line-Height - '+ btnLh + ' Height- ' + btnheight + ' Width-  ' + btnWidth + 'Background-' + $(value).css('background-color'));
-			});
-			// $(cdStructure).each(function(idx, ele){
-			// 	$(ele).append('Font-size- ' + btnFs + ' Line-Height - '+ btnLh + ' Height- ' + btnheight + ' Width-  ' + btnWidth);
-			// })
+		btnBG  = $(value).data('bg'),
+		btnFs  = $(value).css('font-size'),
+		btnLh  = $(value).css('line-height'),
+		btnheight  = $(value).css('height'),
+		btnWidth  = $(value).css('width');
+		$(value).mouseover(function() {
+			$(cdStructure).text('');
+			$(cdStructure).append('Font-size- ' + btnFs + ' Line-Height - '+ btnLh + ' Height- ' + btnheight + ' Width-  ' + btnWidth + 'Background-' + btnBG);
+		});
 	});
 
 
@@ -98,7 +100,7 @@ jQuery(document).ready(function($){
 	$('.cd-typography .headings').each(function(idx,ele){
 		var heading = $(ele),
 			headingDescriptionText = heading.siblings('pre');
-			console.log($(ele).html());
+			//console.log($(ele).html());
 			// body = heading.next('p'),
 			// bodyDescriptionText = body.children('span').eq(0);
 			
@@ -127,6 +129,17 @@ jQuery(document).ready(function($){
 
 	createHtml('#form .cd-box','input');
 
+
+	/*******************
+	UTILITIES
+	********************/
+	$('#utilities .util-structure').each(function(idx,ele){
+		var elementHtml = $(ele).html(),
+		//convertText = $(ele).text(elementHtml),
+		containerHtml = $('<pre><div class="cd-box structure"></div></pre>').insertAfter($(ele));
+		containerHtml.text(elementHtml);
+	})
+	
 
 	/*******************
 	MAIN  NAVIGATION
